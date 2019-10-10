f2f9711d4d3996917e3ba9773fdee55fddcb56a7
Bjorn Cole <bjorn.cole@gmail.com>
2019-10-10 15:48:44 -0400

    still playing2

--- tljh/jupyterhub_config.py
+++ tljh/jupyterhub_config.py
@@ -59,7 +59,7 @@ c.JupyterHub.cleanup_servers = False
 # Use a high port so users can try this on machines with a JupyterHub already present
 c.JupyterHub.hub_port = 15001
 
-#c.JupyterHub.hub_ip "192.168.33.10"
+c.JupyterHub.hub_ip "172.17.0.2"
 
 c.TraefikTomlProxy.should_start = False
 
