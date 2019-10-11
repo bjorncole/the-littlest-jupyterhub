19172ad30220ca30c57df25e8b2756f33acda908
Bjorn Cole <bjorn.cole@gmail.com>
2019-10-11 10:21:23 -0400

    more hub twiddle

--- tljh/jupyterhub_config.py
+++ tljh/jupyterhub_config.py
@@ -60,7 +60,7 @@ c.JupyterHub.cleanup_servers = False
 # Use a high port so users can try this on machines with a JupyterHub already present
 c.JupyterHub.hub_port = 15001
 
-c.JupyterHub.hub_connect_ip = '172.17.0.2'
+#c.JupyterHub.hub_connect_ip = '0.0.0.0'
 
 #c.JupyterHub.hub_ip = '0.0.0.0'
 
