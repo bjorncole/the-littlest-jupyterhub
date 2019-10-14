3e28f2568fdc0fef8e9f7aa80b86ecd63c7c8e1a
Bjorn Cole <bjorn.cole@gmail.com>
2019-10-14 11:47:42 -0400

    think i finally understand

--- tljh/jupyterhub_config.py
+++ tljh/jupyterhub_config.py
@@ -58,11 +58,11 @@ c.DockerSpawner.image = 'jupyter/base-notebook'
 c.JupyterHub.cleanup_servers = False
 
 # Use a high port so users can try this on machines with a JupyterHub already present
-c.JupyterHub.hub_port = 15001
+c.JupyterHub.hub_port = 80
 
 #c.JupyterHub.hub_connect_ip = '0.0.0.0'
 
-#c.JupyterHub.hub_ip = '192.168.33.15'
+c.JupyterHub.hub_ip = '172.17.0.1'
 
 c.TraefikTomlProxy.should_start = False
 
